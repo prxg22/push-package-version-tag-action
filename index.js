78e05d3069ba4e10cd580580261ee99c506d124c
paulo giusti <paulo.giusti@lemon.energy>
2019-11-28 14:32:56 -0300

    fix: remove github dependency

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
 const { exec } = require("@actions/exec");
 const fs = require("fs");
 
 const githubToken = core.getInput("github-token");
-const octokit = new github.GitHub(githubToken);
 
 const getVersion = () => {
   const file = fs.readFileSync("package.json");
@@ -27,7 +25,7 @@ const configGit = async () => {
   await exec(`git config --local user.name "Push Package Version Tag Action"`);
 };
 
-const pushTag = async tag => {
+const pushTag = async () => {
   const actor = process.env.GITHUB_ACTOR;
   const repository = process.env.GITHUB_REPOSITORY;
   const remote = `https://${actor}:${githubToken}@github.com/${repository}.git`;
@@ -43,11 +41,11 @@ const run = async () => {
     const version = getVersion();
     const tag = await createTag(version, prefix);
     if (!tag) {
-      console.log(`> tag already created!`)
-      return
+      console.log(`> tag already created!`);
+      return;
     }
     console.log(`> tag ${tag} created!`);
-    await pushTag(tag);
+    await pushTag();
     console.log(`> tag ${tag} already exist`);
   } catch (e) {
     core.setFailed(e);
